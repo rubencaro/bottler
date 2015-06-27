d2b444828629c2db7d82c7c16f91562205a78a62
Rubén Caro <ruben.caro.estevez@gmail.com>
2015-06-27 19:48:22 +0200

    Version bump

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Bottler.Mixfile do
 
   def project do
     [app: :bottler,
-     version: "0.4.1",
+     version: "0.5.0",
      elixir: "~> 1.0.0",
      package: package,
      description: """
