ca5a40d3f3762a3ec57e1ff821144b17764a8be5
Rubén Caro <ruben.caro.estevez@gmail.com>
2016-05-15 15:19:31 +0200

    observer task first attempt on production

--- lib/mix/tasks/bottler.ex
+++ lib/mix/tasks/bottler.ex
@@ -183,30 +183,27 @@ defmodule Mix.Tasks.Bottler.Observer do
     H.set_prod_environment
     c = H.read_and_validate_config
     c |> inspect |> IO.puts
-    port = get_port(c, args)
-    IO.puts port
-
+    
     ip = c[:servers][name][:ip]
+    port = get_port(c, ip)
 
     # auto closing tunnel
     :os.cmd('killall epmd') # free distributed erlang port
     cmd = "ssh -f -L 4369:localhost:4369 -L #{port}:localhost:#{port} #{c[:remote_user]}@#{ip} sleep 30;" |> to_char_list
-    IO.puts cmd
+    IO.puts "Opening tunnel: #{cmd}"
     :os.cmd(cmd) |> to_string |> IO.puts
-    IO.puts "Tunnel is open."
 
     # observer
     IO.puts "Starting observer..."
-    cmd = "elixir --name observerunique@127.0.0.1 --cookie monikako --no-halt lib/mix/scripts/observer.exs" |> to_char_list
+    cmd = "elixir --name observerunique@127.0.0.1 --cookie monikako --no-halt #{__DIR__}/../../../lib/mix/scripts/observer.exs" |> to_char_list
     IO.puts cmd
     :os.cmd(cmd) |> to_string |> IO.puts
 
     IO.puts "Done"
   end
 
-  defp get_port(c, args) do
-    # cmd = "ssh #{c[:remote_user]}@#{args |> List.first} \"source /home/#{c[:remote_user]}/.bash_profile && epmd -names\" | grep dean | cut -d \" \" -f 5"
-    cmd = "ssh epdp@#{args |> List.first} \"source /home/epdp/.bash_profile && epmd -names\" | grep dean | cut -d \" \" -f 5"
+  defp get_port(c, ip) do
+    cmd = "ssh #{c[:remote_user]}@#{ip} \"source /home/#{c[:remote_user]}/.bash_profile && epmd -names\" | grep dean | cut -d \" \" -f 5"
     :os.cmd(cmd |> to_char_list) |> to_string |> String.strip(?\n)
   end
 end
