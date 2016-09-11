33605c23c9b856eddd2b4250291515b1bf8604cc
Rubén Caro <ruben@elpulgardelpanda.com>
2016-09-11 09:00:43 +0200

    Add mix goto all

--- lib/mix/tasks/goto.ex
+++ lib/mix/tasks/goto.ex
@@ -6,6 +6,7 @@ defmodule Mix.Tasks.Goto do
     Use like `mix goto servername`
 
     It opens an SSH session on a new terminal window on the server with given name.
+    If `all` is given as a server name, then one terminal is open for each configured server.
     The actual `terminal` command can be configured as a template.
 
     `prod` environment is used by default. Use like
@@ -15,25 +16,38 @@ defmodule Mix.Tasks.Goto do
   use Mix.Task
 
   def run(args) do
-    name = args |> List.first |> String.to_atom
-
     H.set_prod_environment
-    c = H.read_and_validate_config |> H.inline_resolve_servers
+    c = H.read_and_validate_config
+      |> H.inline_resolve_servers
 
-    if not name in Keyword.keys(c[:servers]),
-      do: raise "Server not found by that name"
-
-    ip = c[:servers][name][:ip]
-
-    spawn_link(fn->
-      c[:goto][:terminal]
-      |> EEx.eval_string(title: "#{name}", command: "ssh #{c[:remote_user]}@#{ip}")
-      |> to_charlist |> :os.cmd
-    end)
+    args
+    |> get_names(c)
+    |> Enum.map(&open_terminal(&1, c))
 
     Process.sleep(2_000)
 
     :ok
+  end
+
+  defp get_names(args, config) do
+    name = args |> List.first |> String.to_atom
+    case name do
+      :all -> config[:servers] |> Keyword.keys
+      x -> [x]
+    end
+  end
+
+  defp open_terminal(name, config) do
+    if not name in Keyword.keys(config[:servers]),
+      do: raise "Server not found by that name"
+
+    ip = config[:servers][name][:ip]
+
+    spawn_link(fn->
+      config[:goto][:terminal]
+      |> EEx.eval_string(title: "#{name}", command: "ssh #{config[:remote_user]}@#{ip}")
+      |> to_charlist |> :os.cmd
+    end)
   end
 
 end
